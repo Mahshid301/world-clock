9169880688a786b10c29984eed8ec30bcdbded94
Mahshid301 <mahshid.m970@gmail.com>
2025-05-10 12:56:39 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function updateTime() {
   let losAngelesTime = moment().tz("America/Los_Angeles");
   losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
   losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]a[</small]"
+    "h:mm:ss [<small>]A[</small]"
   );
 
   let sydneyElement = document.querySelector("#sydney");
@@ -14,7 +14,7 @@ function updateTime() {
   let sydneyTime = moment().tz("Australia/Sydney");
   sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
   sydneyTimeElement.innerHTML = sydneyTime.format(
-    "h:mm:ss [<small>]a[</small]"
+    "h:mm:ss [<small>]A[</small]"
   );
 }
 updateTime();
